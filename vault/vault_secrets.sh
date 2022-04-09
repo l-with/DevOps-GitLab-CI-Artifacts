88e49c83eba51017775a603b3c6d0856143bb235
Lars With <lars@with.de>
2022-04-09 20:49:21 +0200

    fix

--- vault/vault_secrets.sh
+++ vault/vault_secrets.sh
@@ -6,5 +6,5 @@ cat $1 |\
     | .secrets[] 
     | ("vault write -field=token auth/jwt/login role=\(.VAULT_AUTH_ROLE) jwt=$CI_JOB_JWT >.vault && export VAULT_TOKEN=\"$(cat .vault)\" && rm .vault", 
       (to_entries | map(select(.key != "VAULT_AUTH_ROLE")) | .[] | .key as $var | .value 
-        | "vault kv get "+if has("field") then "-field=\(.field) " else "" end + if has("format") then "-format=\(.format) " else "" end + .path +" >.vault && cp .vault .vault.\(.field) && export " + $var + "=\"$(cat .vault | sed \"s/$/\\n/\")\" && rm .vault"))
+        | "vault kv get "+if has("field") then "-field=\(.field) " else "" end + if has("format") then "-format=\(.format) " else "" end + .path +" >.vault && export " + $var + "=\"$(cat .vault | sed '"'"'s/$/\\n/'"'"')\" && rm .vault"))
   ' -r
