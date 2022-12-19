8f9e64ed5f99cf0b4fe6a38a6cffd45fd2ef9847
Lars With <lars@with.de>
2022-12-19 22:01:42 +0100

    add -mount= for mount: to vault secrets

--- vault/vault_secrets.sh
+++ vault/vault_secrets.sh
@@ -7,7 +7,8 @@ cat $1 |\
     | .secrets[] 
     | ("export VAULT_TOKEN=\"$(vault write -field=token auth/jwt/login role=\(.VAULT_AUTH_ROLE) jwt=$CI_JOB_JWT)\"", 
       (to_entries | map(select(.key != "VAULT_AUTH_ROLE")) | .[] | .key as $var | .value 
-        | "export " + $var + "=\"$(vault kv get "+if has("field") then "-field=\(.field) " else "" end + if has("format") then "-format=\(.format) " else "" end + .path +")\""))
+        | "export " + $var + "=\"$(vault kv get "+ .path + " " + if has("mount") then "-mount=\(.mount) " else "" end + if has("field") then "-field=\(.field) " else "" end + if has("format") then "-format=\(.format) " else "" end +")\""),
+      "unset VAULT_TOKEN")
   ' -r
 elif [ "$2" == "--debug" ] || [ "$2" == "-d" ]; then
 cat $1 |\
@@ -17,7 +18,8 @@ cat $1 |\
     | .secrets[] 
     | ("vault write -field=token auth/jwt/login role=\(.VAULT_AUTH_ROLE) jwt=$CI_JOB_JWT >.vault && export VAULT_TOKEN=\"$(cat .vault)\" && rm .vault", 
       (to_entries | map(select(.key != "VAULT_AUTH_ROLE")) | .[] | .key as $var | .value 
-        | "vault kv get "+if has("field") then "-field=\(.field) " else "" end + if has("format") then "-format=\(.format) " else "" end + .path +" > .vault && echo && export " + $var + "=\"$(cat .vault)\" && rm .vault"))
+        | "vault kv get "+ .path + " " + if has("mount") then "-mount=\(.mount) " else "" end + if has("field") then "-field=\(.field) " else "" end + if has("format") then "-format=\(.format) " else "" end +" > .vault && echo && export " + $var + "=\"$(cat .vault)\" && rm .vault"),
+      "unset VAULT_TOKEN")
   ' -r
 elif [ "$2" == "--test" ] || [ "$2" == "-t" ]; then
 cat $1 |\
@@ -27,7 +29,7 @@ cat $1 |\
     | .secrets[] 
     | ("vault write -field=token auth/jwt/login role=\(.VAULT_AUTH_ROLE) jwt=$CI_JOB_JWT >.vault && export VAULT_TOKEN=\"$(cat .vault)\" && rm .vault", 
       (to_entries | map(select(.key != "VAULT_AUTH_ROLE")) | .[] | .key as $var | .value 
-        | "vault kv get "+if has("field") then "-field=\(.field) " else "" end + if has("format") then "-format=\(.format) " else "" end + .path +" > /dev/null"),
+        | "vault kv get "+ .path + " " + if has("mount") then "-mount=\(.mount) " else "" end + if has("field") then "-field=\(.field) " else "" end + if has("format") then "-format=\(.format) " else "" end +" > /dev/null"),
       "unset VAULT_TOKEN")
   ' -r
 elif [ "$2" == "--markdown" ] || [ "$2" == "-m" ]; then
@@ -46,7 +48,7 @@ cat $1 |\
     | .key as $var 
     | .value 
     | .path as $path
-    | (if has("field") then "-field=\(.field) " else "" end + if has("format") then "-format=\(.format) " else "" end) as $option
+    | (if has("mount") then "-mount=\(.mount) " else "" end + if has("field") then "-field=\(.field) " else "" end + if has("format") then "-format=\(.format) " else "" end) as $option
     | ("| \($role) | \($var) | \($option) | \(.path) |")
   ' -r
 fi
