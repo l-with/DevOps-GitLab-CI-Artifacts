a27ec6678e655e65c7b98354e1bd02835c1ac33d
Lars With <lars@with.de>
2023-01-07 18:41:14 +0100

    fix parameter number

--- vault/vault_token.sh
+++ vault/vault_token.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-if [ "$2" == "" ]; then
+if [ "$1" == "" ]; then
 echo "vault auth role missng"
 else
-VAULT_AUTH_ROLE=$2
+VAULT_AUTH_ROLE=$1
 export VAULT_TOKEN="$(vault write -field=token auth/jwt/login role=$VAULT_AUTH_ROLE jwt=$CI_JOB_JWT)"
 fi
